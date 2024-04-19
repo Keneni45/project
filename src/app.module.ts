ef2282d3409e57dccad5ad012e676af65ce6149f
Keneni45 <60184561+Keneni45@users.noreply.github.com>
2024-04-19 00:34:32 -0700

    Add Database Configration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import Joi from '@hapi/joi';
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [ConfigModule.forRoot({ validationSchema: Joi.object({
-    host:Joi
-  }) })],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        username: Joi.string().required(),
+        host: Joi.string().required(),
+        port: Joi.number().required(),
+        password: Joi.string().required(),
+        database: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('host'),
+        port: configService.get('port'),
+        username: configService.get('username'),
+        database: configService.get('database'),
+        password: configService.get('password'),
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
