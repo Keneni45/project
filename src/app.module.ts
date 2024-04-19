8af0da3bd3d0cdefca45fc904d024d43dc730fd2
Keneni45 <60184561+Keneni45@users.noreply.github.com>
2024-04-19 00:36:06 -0700

    Remove Uncessary Code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from '@hapi/joi';
 
@@ -16,7 +14,5 @@ import * as Joi from '@hapi/joi';
       }),
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
