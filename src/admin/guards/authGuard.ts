a1177334f78af5f8cc0b0912c9b1698a0ca2b908
keneni45 <kenotoneaga@gmail.com>
2024-04-20 13:53:28 -0700

    Add Role Decorator

--- src/admin/guards/authGuard.ts
+++ src/admin/guards/authGuard.ts
@@ -4,6 +4,7 @@ import {
   HttpException,
   HttpStatus,
   Injectable,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { ExpressRequest } from '../types/expressRequest.interface';
 
@@ -11,9 +12,18 @@ import { ExpressRequest } from '../types/expressRequest.interface';
 export class AuthGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest<ExpressRequest>();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
     if (request.admin) {
       return true;
     }
     throw new HttpException('NOT AUTHORIZED', HttpStatus.UNAUTHORIZED);
+  }
+  private extractTokenFromHeader(request: ExpressRequest): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
