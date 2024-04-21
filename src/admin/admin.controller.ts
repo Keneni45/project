9ff9638d27b5511b9c26b354c4c72a84dc590f30
keneni45 <kenotoneaga@gmail.com>
2024-04-21 06:18:44 -0700

    Add Login Functionality

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -8,6 +8,7 @@ import {
 import { AdminService } from './admin.service';
 import { CreateAdminDto } from './dto/createAdmin.dto';
 import { AdminResponseInterface } from './types/adminResponse.interface';
+import { LoginAdminDto } from './dto/loginAdmin.dto';
 
 @Controller()
 export class AdminController {
@@ -15,11 +16,19 @@ export class AdminController {
 
   //admin register request
   @Post('admin')
-  // @UsePipes(new ValidationPipe())
+  @UsePipes(new ValidationPipe())
   async createAdmin(
     @Body('admin') createAdminDto: CreateAdminDto,
   ): Promise<AdminResponseInterface> {
     const admin = await this.adminService.createAdmin(createAdminDto);
+    return this.adminService.buildAdminResponse(admin);
+  }
+  @Post('admin/login')
+  @UsePipes(new ValidationPipe())
+  async loginAdmin(
+    @Body('admin') loginAdminDto: LoginAdminDto,
+  ): Promise<AdminResponseInterface> {
+    const admin = await this.adminService.loginAdmin(loginAdminDto);
     return this.adminService.buildAdminResponse(admin);
   }
 }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -7,6 +7,8 @@ import { JWT_SECRET } from 'config';
 import { sign } from 'jsonwebtoken';
 import { AdminResponseInterface } from './types/adminResponse.interface';
 import { AdminEntity } from './entity/admin.entity';
+import { compare } from 'bcrypt';
+import { LoginAdminDto } from './dto/loginAdmin.dto';
 
 @Injectable()
 export class AdminService {
@@ -55,5 +57,30 @@ export class AdminService {
         token: this.generateJWT(admin),
       },
     };
+  }
+  async loginAdmin(loginAdminDto: LoginAdminDto): Promise<AdminEntity> {
+    const options: FindOneOptions<AdminEntity> = {
+      where: { email: loginAdminDto.email },
+      select: ['id', 'phoneNo', 'password', 'email', 'username'],
+    };
+    const admin = await this.adminRepository.findOne(options);
+    if (!admin) {
+      throw new HttpException(
+        'Credential are not valid',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    const isPasswordCorrect = await compare(
+      loginAdminDto.password,
+      admin.password,
+    );
+    if (!isPasswordCorrect) {
+      throw new HttpException(
+        'Credential are not valid',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    delete admin.password;
+    return admin;
   }
 }
